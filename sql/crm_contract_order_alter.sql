906b5be0432115a426888c39f9775aecc521fc10
hanmou666529 <1947606328@qq.com>
2026-07-03 22:16:58 +0800

    Delete sql/crm_contract_order_alter.sql

--- sql/crm_contract_order_alter.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE crm_contract
-  ADD COLUMN approval_count    INT       DEFAULT 0 COMMENT '累计审批通过次数',
-  ADD COLUMN last_approval_time DATETIME DEFAULT NULL COMMENT '最后一次审批通过时间';
-
-ALTER TABLE crm_order
-  ADD COLUMN source VARCHAR(20) DEFAULT 'manual' COMMENT '订单来源: manual-手动创建, contract-合同生成';
